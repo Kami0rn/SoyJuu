bf31b5664f88f7090f7181868104c20f750b6570
Kami0rn <tew965@gmail.com>
2023-10-08 16:23:33 +0700

    lol

--- backend/entities/rider.go
+++ backend/entities/rider.go
@@ -8,6 +8,27 @@ type Rider struct {
 
 	gorm.Model
 
+	// RideFirstName string
+
+	// RideLastName string
+
+	// RideUsername string `gorm:"unique"`
+
+	// RidePassword string
+
+	// RideTel int
+
+	// RideEmail string `gorm:"unique"`
+
+	// DriverLicense int
+
+	// CarNum string
+
+
+	//FK export
+	// Deliveries []Delivery `gorm:"foreignKey:RiderID"`
+
+	// Salarys []Salary `gorm:"foreignKey:RiderID"`
 
 
 }
