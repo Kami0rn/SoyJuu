d95b6f321b60498836026fbc434f09075e447f2f
Kami0rn <tew965@gmail.com>
2023-10-09 12:29:08 +0700

    --- Re project ---

---
+++ backend/entity/customer.go
@@ -0,0 +1,23 @@
+package entity
+
+
+import (
+
+	"gorm.io/gorm"
+	
+	)
+	
+	
+	type User struct {
+	
+	gorm.Model
+	
+	FirstName string
+	
+	LastName string
+	
+	Email string
+	
+	Phone string
+	
+	}
